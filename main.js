cbd9f13920839aa17bdbbee50d161ee33d48160f
hungprok <57722641+hungprok@users.noreply.github.com>
2020-03-11 08:32:26 +0700

    third

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 let InputValue = document.getElementById("todoinput");
 
-let todoList = JSON.parse(localStorage.getItem('todoList'));;
+// let todoList = JSON.parse(localStorage.getItem('todoList'));;
+let todoList = [];
 let isDone = false;
 let status = "DONE"
 let checkboxStatus = false;
@@ -17,16 +18,16 @@ let addItem = () => {
 }
 
 let Render = () => {
-    // console.log(JSON.parse(localStorage.getItem("todoList")));
-    let HTMLtodoArray = todoList.map((item, index) => {
-        return `<li style="text-decoration:${item.isDone ?"Line-through": ""}" > ${item.value} ${item.isDone} <button class="btn btn-danger" onclick = "remove (${index})">
+        // console.log(JSON.parse(localStorage.getItem("todoList")));
+        let HTMLtodoArray = todoList.map((item, index) => {
+            return `<li style="text-decoration:${item.isDone ?"Line-through": ""}" > ${item.value} ${item.isDone} <button class="btn btn-danger" onclick = "remove (${index})">
                 X </button><button class="btn btn-success" id="DoneButton" onclick = "Done (${index})">${item.status}</button></li > `;
-    }).join("");
-    document.getElementById("resultArea").innerHTML = HTMLtodoArray;
-    console.log(todoList);
-    localStorage.setItem("todoList", JSON.stringify(todoList));
-}
-Render();
+        }).join("");
+        document.getElementById("resultArea").innerHTML = HTMLtodoArray;
+        console.log(todoList);
+        // localStorage.setItem("todoList", JSON.stringify(todoList));
+    }
+    // Render();
 
 let remove = (index) => {
     todoList.splice(index, 1);
@@ -37,9 +38,9 @@ let Done = (index) => {
     todoList[index].isDone = !(todoList[index].isDone);
     if (todoList[index].isDone) { todoList[index].status = "DO THIS MAN" } else { todoList[index].status = 'DONE' }
     document.getElementById("DoneButton").innerHTML = todoList[index].status;
-    if (todoList[index].isDone) {
-        document.getElementById(`${index}`).style.setProperty("text-decoration", "line-through");
-    }
+    // if (todoList[index].isDone) {
+    //     document.getElementById(`${index}`).style.setProperty("text-decoration", "line-through");
+    // }
     Render();
 }
 
@@ -54,8 +55,8 @@ let ShowDone = () => {
         var filtered = todoList.filter(filter_done);
         console.log(filtered);
         let HTMLtodoArray = filtered.map((item, index) => {
-            return `<li id=${index} > ${item.value} ${item.isDone} <button onclick = "remove (${index})">
-                X </button><button id="DoneButton" onclick = "Done (${index})">${item.status}</button></li > `;
+            return `<li style="text-decoration:${item.isDone ?"Line-through": ""}" > ${item.value} ${item.isDone} <button class="btn btn-danger" onclick = "remove (${index})">
+                X </button><button class="btn btn-success" id="DoneButton" onclick = "Done (${index})">${item.status}</button></li > `;
         }).join("");
         document.getElementById("resultArea").innerHTML = HTMLtodoArray;
     } else { Render() }
